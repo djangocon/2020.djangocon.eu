f09b1abf623c4f8f3cacd0194a5f3f512dad154b
Victor Duarte <vduarte@evolutio.pt>
2019-09-17 12:40:25 +0100

    Production

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["2020.djangocon.eu"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["2020.djangocon.eu",'176.31.24.157'])
 
 # DATABASES
 # ------------------------------------------------------------------------------
