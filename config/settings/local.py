001a260f6325fbc765a892b036342dce61c5927d
Victor Duarte <vduarte@evolutio.pt>
2019-09-17 13:04:18 +0100

    Production

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1",'176.31.24.157']
-
+SECURE_SSL_REDIRECT = False
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
